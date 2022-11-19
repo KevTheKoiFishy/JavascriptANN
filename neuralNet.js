f8444a9e77b132a4099b4c359db5b4d2ac2d17ce
Glitch (glitch-hello-website) <none>
2022-11-19 10:26:00 +0000

    🌔🏩 Checkpoint ./neuralNet.js:29312057/1039

--- neuralNet.js
+++ neuralNet.js
@@ -1,4 +1,4 @@
-var datagrid;
+var datagrid = [];
 var numLayers  = 4;
 var numValues = [datagrid.length, 16, 16, 10];
 var NN = [];
@@ -6,16 +6,27 @@ var values = [datagrid];
 
 var layer = 0;
 
+//make
 for (layer = 1; layer < numLayers; ++layer){
   NN[layer] = [];
-  datagrid[layer] = []
+  values[layer] = []
   for (var val = 0; val < numValues[layer]; ++val){
     NN[layer][val] = [];
-    datagrid[layer][val] = [];
+    values[layer][val] = 0;
     for (var input = 0; input < numValues[layer-1]; ++input){
       NN[layer][val][input] = [Math.random(), Math.random()];
     }
   }
 }
 
-console.log(NN);
+//run
+for (layer = 1; layer < numLayers; ++layer){
+  for (var val = 0; val < numValues[layer]; ++val){
+    for (var input = 0; input < numValues[layer-1]; ++input){
+      values[layer][val] += values[layer-1][input] * NN[layer][val][input][0] - NN[layer][val][input][1];
+      if (values[layer][val] < 0) { values[layer][val] = 0; }
+    }
+  }
+}
+
+console.log(values);
