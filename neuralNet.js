23ea617021a3961617885721cc5c94981d9df41d
Glitch (glitch-hello-website) <none>
2022-11-19 10:13:15 +0000

    🦃🏌 Checkpoint ./neuralNet.js:29312057/1338 ./test.html:29312057/182

--- neuralNet.js
+++ neuralNet.js
@@ -1,15 +1,21 @@
+var datagrid;
 var numLayers  = 4;
-var numNeurons = [datagrid.length, 16, 16, 10];
+var numValues = [datagrid.length, 16, 16, 10];
 var NN = [];
 var values = [datagrid];
 
 var layer = 0;
 
-for (layer = 1; layer < numLayers; ++layer)
-{
-  NN.push([]);
-  for (var neuron = 0; neuron < numNeurons[layer]; ++neuron)
-  {
-    NN[layer].push([Math.Random(), Math.Random()])
+for (layer = 1; layer < numLayers; ++layer){
+  NN[layer] = [];
+  datagrid[layer] = []
+  for (var val = 0; val < numValues[layer]; ++val){
+    NN[layer][val] = [];
+    datagrid[layer][val] = [];
+    for (var input = 0; input < numValues[layer-1]; ++input){
+      NN[layer][val][input] = [Math.random(), Math.random()];
+    }
   }
 }
+
+console.log(NN);
