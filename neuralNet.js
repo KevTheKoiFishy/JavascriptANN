e7c20fc8309bd8a33352ee778d5748f22c4dc0a0
Glitch (glitch-hello-website) <none>
2022-11-19 10:38:12 +0000

    🚕🎻 Checkpoint ./neuralNet.js:29312057/1108

--- neuralNet.js
+++ neuralNet.js
@@ -1,4 +1,4 @@
-var datagrid = [];
+var datagrid = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,1,1,1,1,1,0,0,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 var numLayers  = 4;
 var numValues = [datagrid.length, 16, 16, 10];
 var NN = [];
@@ -14,7 +14,7 @@ for (layer = 1; layer < numLayers; ++layer){
     NN[layer][val] = [];
     values[layer][val] = 0;
     for (var input = 0; input < numValues[layer-1]; ++input){
-      NN[layer][val][input] = [Math.random(), Math.random()];
+      NN[layer][val][input] = [Math.random()/numValues[layer-1], Math.random()];
     }
   }
 }
@@ -23,10 +23,8 @@ for (layer = 1; layer < numLayers; ++layer){
 for (layer = 1; layer < numLayers; ++layer){
   for (var val = 0; val < numValues[layer]; ++val){
     for (var input = 0; input < numValues[layer-1]; ++input){
-      values[layer][val] += values[layer-1][input] * NN[layer][val][input][0] - NN[layer][val][input][1];
-      if (values[layer][val] < 0) { values[layer][val] = 0; }
-    }
-  }
-}
+      if (values[layer][val] > NN[layer][val][input][1]){
+        values[layer][val] += (values[layer-1][input] - NN[layer][val][input][1]) * NN[layer][val][input][0];
+} } } }
 
 console.log(values);
