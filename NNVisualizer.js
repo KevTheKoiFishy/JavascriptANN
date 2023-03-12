7d8d4ec7d331b306c94e07325cbb96fed9cf739b
Glitch (glitch-hello-website) <none>
2023-03-12 20:02:03 +0000

    🍜🚈 Checkpoint ./main.css:29312057/826 ./NNVisualizer.js:29312057/368

--- NNVisualizer.js
+++ NNVisualizer.js
@@ -1,11 +1,17 @@
 var visual = document.createElement("DIV");
     gridContainer.appendChild(visual);
+    visual.id = "NNVisual";
     for (var Nlayer = 0; Nlayer < nodesByLayer.length; ++Nlayer){
         var thisLayer = visual.appendChild(document.createElement("DIV"));
             thisLayer.id = "L" + Nlayer;
+            thisLayer.align = "center";
         for (var Nnode = 0; Nnode < nodesByLayer[Nlayer]; ++Nnode){
-            var thisNode = thisLayer.appendChild(document.createElement("SPAN"));
+            var thisNode = thisLayer.appendChild(document.createElement("DIV"));
                 thisNode.id = "L" + Nlayer + "_N" + Nnode;
-                thisNode.innerText = Nnode;
+                if (nodesByLayer[Nlayer] < 50){
+                  thisNode.innerText = Nnode;
+                } else {
+                  
+                }
         }
     }
