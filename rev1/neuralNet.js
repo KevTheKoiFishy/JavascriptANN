057e1db3f63c8dcdafcf1a05d282bb3e83395a91
Glitch (glitch-hello-website) <none>
2023-07-05 15:16:12 +0000

    💏👟 Checkpoint ./rev1/neuralNet.js:29312057/14

--- rev1/neuralNet.js
+++ rev1/neuralNet.js
@@ -21,7 +21,7 @@ function ReLU(prevLayer, weights, bias){
 //     [{value: undefined, Z: undefined, weights: [0.5, 0.5], bias: 0}],
 // ];
 var gridWidth, gridHeight, gridSize;
-var nodesByLayer = [gridSize, 10, 10];
+var nodesByLayer = [gridSize, 10];
   var numLayers = nodesByLayer.length;
 var NN = [];
 function initNN(){
